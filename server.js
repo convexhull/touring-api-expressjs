951e5a88398f0fb2be9ff9907824640fbb86663d
Yash Pratap Singh <singhyashpratap06@gmail.com>
2024-07-06 18:31:59 +0530

    Setup mongoose and connect to atlas db

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB_URI = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+// BEST METHOD for connection. Using IIFE with async-await
+(async () => {
+  try {
+    const conn = await mongoose.connect(DB_URI);
+    console.log(conn);
+    console.log('DB connection successful');
+  } catch (e) {
+    console.log(e.message);
+  }
+})();
+
+// Second BEST METHOD using async-await and .then/.catch on async() promise
+// async function connectDb() {
+//   const conn = await mongoose.connect(DB_URI);
+//   return conn;
+// }
+
+// connectDb()
+//   .then((conn) => {
+//     console.log(conn);
+//   })
+//   .catch((e) => {
+//     console.log(e.message);
+//   });
 
 // Start Server
 const port = process.env.PORT || 3000;
