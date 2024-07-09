1f9e8c5b12854886a32f4bcf92a305d0a382d93c
Yash Pratap Singh <singhyashpratap06@gmail.com>
2024-07-09 13:05:44 +0530

    Add API features: filtering, sorting, projecting, pagination and route aliasing using middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,70 @@
 const Tour = require('../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    // 1A) FILTERING
+    const excludedFields = ['sort', 'page', 'limit', 'fields'];
+    let queryObj = { ...req.query };
+    excludedFields.forEach((field) => delete queryObj[field]);
+
+    // 1B) ADVANCED FILTERING
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(
+      /(\b(gte|lte|gt|lt)\b)/g,
+      (match) => `$${match}`,
+    );
+    queryObj = JSON.parse(queryStr);
+    let query = Tour.find(queryObj);
+
+    // 2) SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 3) FIELD LIMITING
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4) PAGINATION
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 3;
+    const skip = (page - 1) * limit;
+
+    if (req.query.page) {
+      const totalTours = await Tour.countDocuments();
+      if (skip >= totalTours) {
+        throw new Error('This page does not exist');
+      }
+    }
+    query = query.skip(skip).limit(limit);
+
+    // ALTERNATE QUERYING WAY
+    // const query = Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
