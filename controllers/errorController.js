bce35197592631a2a18661d80dc1c4cb2c58e05e
Yash Pratap Singh <singhyashpratap06@gmail.com>
2024-07-18 11:36:13 +0530

    Add separate error messages for prod/dev, prevent leaking sensitive error info on prod

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,71 @@
-module.exports = (err, req, res, next) => {
-  //   console.log(err.stack);
+const AppError = require('../utils/appError');
 
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errorResponse.errmsg.match(/"(.*?)"/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value.`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  // In recent mongoose, error msgs have become much better. Actually can also use err.message from mongoose in this validation case
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
-    // isOperational: err.isOperational,
+    stack: err.stack,
+    error: err,
   });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational trusted error, we only send operatoin error messages to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  // Programming or other unknown error: don't leak error details
+  else {
+    // 1) Log error
+    console.error('ERRRR: ', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  //   console.log(err.stack);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    if (err.name === 'CastError') {
+      error = handleCastErrorDB(err);
+    }
+    if (err.code === 11000) {
+      error = handleDuplicateFieldsDB(err);
+    }
+    if (err.name === 'ValidationError') {
+      error = handleValidationErrorDB(err);
+    }
+    sendErrorProd(error, res);
+  }
 };
